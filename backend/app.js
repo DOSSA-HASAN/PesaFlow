4ccd223d2be29b770365097bd5cc480271c5f93a
Hasan <hassandossa5@gmail.com>
2026-02-26 21:52:57 +0300

    feat: added database connection import

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config.js"
 import userRoute from "./routes/user.route.js";
+import {connectDb} from "./utils/dbConnection.js";
 
 const app = express()
 
@@ -9,6 +10,8 @@ app.use(cors({
     origin: "*",
     credentials: true
 }))
+
+connectDb()
 
 // Routes
 app.use("/api/user", userRoute)
