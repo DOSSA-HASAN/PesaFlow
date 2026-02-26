4185b109f4be51b76148cfcda25bbdf5f46a8653
Hasan <hassandossa5@gmail.com>
2026-02-26 21:52:02 +0300

    refactor: fixed errors

--- backend/models/tillNumber.model.js
+++ backend/models/tillNumber.model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const tillModel = new mongoose.Schema({
-    tillNumber: {type:String, unique, required},
-    branchName: {type:String, required}
+    tillNumber: {type: String, unique: true, required: true},
+    branchName: {type: String, required: true}
 })
 
 const Till = mongoose.model("Till", tillModel)
