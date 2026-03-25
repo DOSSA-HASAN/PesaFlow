04fe23db76ed1d25f5482213b9fded3ef398ce95
Hasan <hassandossa5@gmail.com>
2026-03-25 11:41:00 +0300

    feat: added email worker

---
+++ backend/workers/email.worker.js
@@ -0,0 +1,24 @@
+import {connectRabbitMQ} from "../events/connection.js";
+import {AppError} from "../utils/AppError.js";
+
+const startWorker = async () => {
+    const channel = await connectRabbitMQ()
+    const queue = "SEND_EMAIL"
+
+    await channel.assertQueue(queue, {durable: true})
+
+    channel.consume(queue, async (msg) => {
+        const data = JSON.parse(msg.content.toString())
+        try {
+            // TODO: send email using nodemail
+            console.log(`Sending email to: ${data.email}`)
+
+            channel.ack(msg)
+        } catch (e){
+            console.error(`❌ Failed to send email:\n${e.message}`)
+            channel.nack(msg, false, false)
+        }
+    })
+}
+
+startWorker()
