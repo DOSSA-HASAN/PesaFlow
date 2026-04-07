8701cc4dd47fc15753e21706d68dda9a23e2560f
Hasan <hassandossa5@gmail.com>
2026-04-07 14:18:49 +0300

    refactor: changed function to match with current RBAC architecture

---
+++ backend/middlewares/checkRole.middleware.js
@@ -0,0 +1,20 @@
+import {Permission} from "../rbac/permission.model.js";
+import {errorResponse} from "../utils/response.js";
+
+/**
+ *
+ * @param requiredPermission an array that includes the permissions for each route
+ */
+export const authorize = (requiredPermission) => {
+    return async (req, res, next) => {
+        const user = req.user
+        const userRoles = await user.getRoles({include: Permission})
+
+        const userPermissions = userRoles.flatMap(role => role.Permissions.map(permission => permission.key))
+
+        if(!userPermissions.includes(requiredPermission)){
+            return errorResponse(res, "you do not have this permission", 403)
+        }
+        next()
+    }
+}
