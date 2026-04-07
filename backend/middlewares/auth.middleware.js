20e1f722cf517ae62bb59df46b9125046608b3b3
Hasan <hassandossa5@gmail.com>
2026-04-07 14:19:18 +0300

    feat: added function to verify jwt token from user

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+// check for users access token from auth header make sure it starts with Bearer, decode it
+// if decoding was success return the user and
+
+import {errorResponse} from "../utils/response.js";
+import jwt from "jsonwebtoken"
+
+export const verifyUser = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if (!authHeader) {
+        return errorResponse(res, "Missing authentication header", 401)
+    }
+
+    if (!authHeader.startsWith("Bearer ")) {
+        return errorResponse(res, "Invalid authentication header", 401)
+    }
+
+    try {
+        const token = authHeader.split(" ")[1]
+        const JWT_ACCESS_TOKEN_SECRET = process.env.ACCESS_SECRET
+        const decoded = jwt.verify(token, JWT_ACCESS_TOKEN_SECRET)
+        if (!decoded) {
+            return errorResponse(res, "Invalid token", 401)
+        }
+
+        req.user = decoded
+        next()
+    } catch (e) {
+        return errorResponse(res, "Invalid or expired token", 401, e)
+    }
+}
