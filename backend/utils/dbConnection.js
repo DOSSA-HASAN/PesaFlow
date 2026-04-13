2db5472760dc0724e4b5b10dc65dacf94171f8d1
Hasan <hassandossa5@gmail.com>
2026-04-13 11:17:19 +0300

    refactor: minor adjustments to console.logs

--- backend/utils/dbConnection.js
+++ backend/utils/dbConnection.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
     try {
-        const DATABASE_CONNECTION_URL = process.env.DB_URL
+        const DATABASE_CONNECTION_URL = process.env.MONGODB_URL
         await mongoose.connect(DATABASE_CONNECTION_URL)
-        console.log("Connected to database ✅")
+        console.log("Connected to mongoDB ✅")
     } catch (e) {
-        console.log(`❌ Failed to connect to database: ${e} ❌`)
+        console.log(`❌ Failed to connect to mongoDB: ${e} ❌`)
         process.exit(1)
     }
 }
