08a0adc5b19e8bd286457104f7948a8d877a6eec
Hasan <hassandossa5@gmail.com>
2026-04-13 11:16:50 +0300

    feat: added meta field to logs

--- backend/utils/mongoLogger.js
+++ backend/utils/mongoLogger.js
@@ -1,19 +1,17 @@
 import mongoose from "mongoose";
+import {AppError} from "./AppError.js";
 
 const logSchema = new mongoose.Schema({
-    tenantId: String,
     message: String,
-    date: {
+    timing: Number,
+    meta: {
+        type: Object,
+        default: {}
+    },
+    createdAt: {
         type: Date,
         default: Date.now
     }
 })
 
-const Log = mongoose.model("Log", logSchema)
-
-export const mongoLogger = async (tenantId, message) => {
-    await Log.create({
-        tenantId,
-        message
-    })
-}
+export const Log = mongoose.model("Log", logSchema)
