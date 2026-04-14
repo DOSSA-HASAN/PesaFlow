19bea57d9267490c7af4e118ac81b9bbfc85ba46
Hasan <hassandossa5@gmail.com>
2026-04-14 11:19:00 +0300

    feat: added service function to fetch all roles

--- backend/rbac/role/role.service.js
+++ backend/rbac/role/role.service.js
@@ -14,3 +14,15 @@ export const addRole = async (roleName) => {
         return {ok: false, message: e.message || "Failed to created role"}
     }
 }
+
+export const getAllRoles = async () => {
+    try {
+        const roles = await Roles.findAll()
+        if (!roles) {
+            return {ok: false, message: "Failed to fetch roles"}
+        }
+        return {ok: true, message: "Roles fetch successfully", data: roles}
+    } catch (e) {
+        return {ok:false, message: "An error occurred while fetching roles"}
+    }
+}
