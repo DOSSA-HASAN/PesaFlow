278500d9fbb17341a13da8139391b3b092ffa3c5
Hasan <hassandossa5@gmail.com>
2026-04-14 08:46:00 +0300

    feat: added controller function to get permission by id, and linked it with service layer

--- backend/rbac/permission.controller.js
+++ backend/rbac/permission.controller.js
@@ -21,8 +21,23 @@ export const getAllPermissions = async (req, res) => {
             return errorResponse(res, permissions.message, 404)
         }
         return successResponse(res, permissions.data, permissions.message || "Permissions fetched successfully")
-    } catch (e){
+    } catch (e) {
         return errorResponse(res, e.message || "Failed to fetch permissions", 500)
     }
 }
 
+export const getPermissionById = async (req, res) => {
+    const {id} = req.params
+    try {
+        if (!id) return errorResponse(res, "Missing ID param", 400)
+
+        const permission = await permissionService.getPermissionById(id)
+        if (!permission.ok) {
+            return errorResponse(res, permission.message || "Failed to fetch permission", 400)
+        }
+
+        return successResponse(res, permission.data, permission.message || "Fetched permission successfully", 201)
+    } catch (e) {
+        return errorResponse(res, e.message, 500)
+    }
+}
