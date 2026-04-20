74668945911b6c3d1ec806cb9996e1f24a6e8a18
Hasan <hassandossa5@gmail.com>
2026-04-20 09:31:06 +0300

    feat: add role management endpoints

--- backend/rbac/role/role.route.js
+++ backend/rbac/role/role.route.js
@@ -3,6 +3,11 @@ import * as roleController from "./role.controller.js"
 
 const router = express.Router()
 
-router.post('/add', roleController.addRole)
+router.post('/', roleController.addRole)
+router.get('/', roleController.getAllRoles)
+router.get('/:field', roleController.getRoleByIdOrName)
+router.delete('/:id', roleController.deleteRole)
+router.patch('/:id', roleController.updateRole)
+router.post('/:id/permissions', roleController.assignPermission)
 
 export default router
