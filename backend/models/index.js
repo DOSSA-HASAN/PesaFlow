46affdfa317e1be125be105ad4b0183e0fb91763
Hasan <hassandossa5@gmail.com>
2026-04-20 10:53:47 +0300

    feat: define database associations for RBAC

---
+++ backend/models/index.js
@@ -0,0 +1,18 @@
+import Tenant from "../tenant/tenant.model.js";
+import User from "../user/user.model.js";
+import {Role} from "../rbac/role/role.model.js";
+import {Permission} from "../rbac/permission/permission.model.js";
+import {sequelize} from "../config/db.js";
+
+// Tenant.hasMany(User, {foreignKey: "tenantId", as: "users"})
+// User.belongsTo(Tenant, {foreignKey: "tenantId", as: "tenant"})
+
+User.belongsToMany(Role, {through: "UserRoles"})
+Role.belongsToMany(User, {through: "UserRoles"})
+
+const RolePermissions = sequelize.define("RolePermission", {}, {timestamps: true})
+
+Role.belongsToMany(Permission, {through: RolePermissions})
+Permission.belongsToMany(Role, {through: RolePermissions})
+
+export {Tenant, User, Role, Permission, RolePermissions}
