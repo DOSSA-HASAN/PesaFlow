956b1b48c50e940ff43914501bb4e4a1cbd31631
Hasan <hassandossa5@gmail.com>
2026-04-20 08:40:02 +0300

    refactor: standardize error handling and add getAllRoles service

--- backend/rbac/role/role.controller.js
+++ backend/rbac/role/role.controller.js
@@ -1,21 +1,40 @@
 import * as roleService from "./role.service.js"
 import {errorResponse, successResponse} from "../../utils/response.js";
+import {AppError} from "../../utils/AppError.js";
 
-export const addRole = async (req, res) => {
+export const addRole = async (req, res, next) => {
     try {
         const {roleName} = req.body
         if (!roleName) {
-            return errorResponse(res, "Missing required role field", 400)
+            throw new AppError("Missing required role field", 400)
         }
 
-        const role = await roleService.addRole(roleName)
-        if (!role.ok) {
-            return errorResponse(res, role.message || "Failed to add role", 400)
-        }
+        await roleService.addRole(roleName)
 
-        return successResponse(res, null, role.message || "Role create successfully", 201)
+        return successResponse(res, null, "Role created successfully", 201)
     } catch (e) {
-        console.error(`An error occurred while adding a role: ${e.message}`)
-        return errorResponse(res, e.message || "Internal Server Error", e.statusCode || 500)
+        next(e)
+    }
+}
+
+export const getAllRoles = async (req, res, next) => {
+    try {
+        const roles = await roleService.getAllRoles()
+        return successResponse(res, roles, "Roles fetched successfully", 200)
+    } catch (e) {
+        next(e)
+    }
+}
+
+export const getRoleByIdOrName = async (req, res, next) => {
+    try {
+        const {field} = req.params
+
+        const role = await roleService.getRoleByIdOrName(field)
+
+        return successResponse(res, role, "Role fetched successfully", 200)
+
+    } catch (e) {
+        next(e)
     }
 }
