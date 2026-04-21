cecd8fc5f25e9170b6d6312feae5d6f5463e4049
Hasan <hassandossa5@gmail.com>
2026-04-21 17:44:07 +0300

    refactor: use controller alias

--- backend/user/user.route.js
+++ backend/user/user.route.js
@@ -1,15 +1,15 @@
 import express from "express"
-import {createUser, deleteUser, getAllUsers, getUser, updateUserProfile} from "./user.controller.js";
-import {verifyUser} from "../middlewares/auth.middleware.js";
-import {authorize} from "../middlewares/checkRole.middleware.js";
+import * as userController from "./user.controller.js";
+import { verifyUser } from "../middlewares/auth.middleware.js";
+import { authorize } from "../middlewares/checkRole.middleware.js";
 
 const router = express.Router()
 // ensure developer / admin has a valid accessToken
-router.use(verifyUser, authorize(["DEVELOPER", "ADMIN"]))
-router.post("/", createUser)
-router.patch("/:id", updateUserProfile)
-router.delete("/:id", deleteUser)
-router.get("/", getAllUsers)
-router.get("/:id", getUser)
+router.use(verifyUser, authorize(["developer", "admin"]))
+router.post("/", userController.createUser)
+router.get("/", userController.getAllUsers)
+router.patch("/:id", userController.updateUserProfile)
+router.delete("/:id", userController.deleteUser)
+router.get("/:id", userController.getUser)
 
 export default router
