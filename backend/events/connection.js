67fa65148fd04d228fd3c8a781501582349b1f60
Hasan <hassandossa5@gmail.com>
2026-04-21 17:32:17 +0300

    refactor: console warn if channel not present

--- backend/events/connection.js
+++ backend/events/connection.js
@@ -1,5 +1,5 @@
 import amqp from "amqplib"
-import {AppError} from "../utils/AppError.js";
+import { AppError } from "../utils/AppError.js";
 
 let channel;
 
@@ -10,15 +10,17 @@ export const connectRabbitMQ = async () => {
 
     console.log("Connected to RabbitMQ ✅")
 
-    await channel.assertQueue("SEND_EMAIL", {durable: true})
-    await channel.assertQueue("SEQUELIZE_LOGS", {durable: true})
+    await channel.assertQueue("SEND_EMAIL", { durable: true })
+    await channel.assertQueue("SEQUELIZE_LOGS", { durable: true })
 
     return channel
 }
 
 export const getChannel = () => {
     if (!channel) {
-        throw new AppError("RabbitMQ not connected", 500)
+        // throw new AppError("RabbitMQ not connected", 500)
+        console.warn("RabbitMQ not ready")
+        return null
     }
     return channel
 }
