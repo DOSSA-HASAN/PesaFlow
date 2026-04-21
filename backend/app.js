399a9b3171fc5d920f19a97ca28a9e61d77dc4b3
Hasan <hassandossa5@gmail.com>
2026-04-21 17:29:34 +0300

    feat: add auth router, add models association import

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import "./models/index.js"
 import express from "express"
 import cors from "cors"
 import "dotenv/config.js"
@@ -8,8 +9,11 @@ import { connectSQL } from "./config/db.js";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
 import permissionRoute from "./rbac/permission/permission.route.js"
 import roleRoute from "./rbac/role/role.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express()
+
+app.use(express.json())
 
 app.use(cors({
     origin: "*",
@@ -23,6 +27,7 @@ await connectSQL()
 
 
 // Routes
+app.use("/api/auth", authRoute)
 app.use("/api/user", userRoute)
 app.use("/api/permissions", permissionRoute)
 app.use("/api/roles", roleRoute)
