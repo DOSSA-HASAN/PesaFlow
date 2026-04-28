afe0b6cae216f37bfe006d1c25f18683171590c8
Hasan <hassandossa5@gmail.com>
2026-04-28 18:42:51 +0300

    code alignment

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,17 +1,17 @@
 import User from "../user/user.model.js";
-import {Role} from "../rbac/role/role.model.js";
-import {Permission} from "../rbac/permission/permission.model.js";
-import {sequelize} from "../config/db.js";
+import { Role } from "../rbac/role/role.model.js";
+import { Permission } from "../rbac/permission/permission.model.js";
+import { sequelize } from "../config/db.js";
 
 // Tenant.hasMany(User, {foreignKey: "tenantId", as: "users"})
 // User.belongsTo(Tenant, {foreignKey: "tenantId", as: "tenant"})
 
-User.belongsToMany(Role, {through: "UserRoles"})
-Role.belongsToMany(User, {through: "UserRoles"})    
+User.belongsToMany(Role, { through: "UserRoles" })
+Role.belongsToMany(User, { through: "UserRoles" })
 
-const RolePermissions = sequelize.define("RolePermission", {}, {timestamps: true})
+const RolePermissions = sequelize.define("RolePermission", {}, { timestamps: true })
 
-Role.belongsToMany(Permission, {through: RolePermissions})
-Permission.belongsToMany(Role, {through: RolePermissions})
+Role.belongsToMany(Permission, { through: RolePermissions })
+Permission.belongsToMany(Role, { through: RolePermissions })
 
-export {User, Role, Permission, RolePermissions}
+export { User, Role, Permission, RolePermissions }
