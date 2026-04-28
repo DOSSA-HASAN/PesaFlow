84d1a53afde62bb48a2de4ab3666ece36bde1fd6
Hasan <hassandossa5@gmail.com>
2026-04-28 18:33:52 +0300

    feat: add function to set user roles

--- backend/user/user.controller.js
+++ backend/user/user.controller.js
@@ -1,19 +1,19 @@
-import {errorResponse, successResponse} from "../utils/response.js";
+import { errorResponse, successResponse } from "../utils/response.js";
 import * as userService from "./user.service.js"
-import {publishEvent} from "../events/publisher.js";
-import {AppError} from "../utils/AppError.js";
+import { publishEvent } from "../events/publisher.js";
+import { AppError } from "../utils/AppError.js";
 
 
 export const createUser = async (req, res, next) => {
     try {
         // Extract required fields from request body
-        const {email, password} = req.body
+        const { email, password, roleId } = req.body
 
-        if(!email || !password){
+        if (!email || !password || !roleId) {
             throw new AppError("Missing required fields", 400)
         }
 
-        const user = await userService.createUser({email, password})
+        const user = await userService.createUser({ email, password, roleId })
 
         publishEvent("SEND_EMAIL", user)
 
@@ -25,11 +25,11 @@ export const createUser = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
     try {
-        const {email, password} = req.body
-        if(!email || !password){
+        const { email, password } = req.body
+        if (!email || !password) {
             throw new AppError("Missing required credentials", 400)
         }
-        const data = await userService.login({email, password})
+        const data = await userService.login({ email, password })
         return successResponse(res, data, "Login successful", 200)
     } catch (e) {
         next(e)
@@ -38,13 +38,13 @@ export const login = async (req, res, next) => {
 
 export const updateUserProfile = async (req, res, next) => {
     try {
-        const {id} = req.params
-        const {data} = req.body
-        if(!data){
+        const { id } = req.params
+        const { data } = req.body
+        if (!data) {
             throw new AppError("Missing required data", 400)
         }
-        await  userService.updateUserProfile(id, data)
-        return successResponse(res, null, "User updated successfully", 200)
+        const updateUser = await userService.updateUserProfile(id, data)
+        return successResponse(res, updateUser, "User updated successfully", 200)
     } catch (e) {
         next(e)
     }
@@ -52,7 +52,7 @@ export const updateUserProfile = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
     try {
-        const {id} = req.params
+        const { id } = req.params
         const deletedUser = await userService.deleteUser(id)
         return successResponse(res, null, "User deleted successfully", 200)
     } catch (e) {
@@ -71,9 +71,28 @@ export const getAllUsers = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
     try {
-         const {id} = req.params
+        const { id } = req.params
         const user = await userService.getUser(id)
         return successResponse(res, user, "Success", 200)
+    } catch (e) {
+        next(e)
+    }
+}
+
+export const setUserRoles = async (req, res, next) => {
+    try {
+        const { id } = req.user
+        const { roleIds } = req.body
+
+        if (!id) {
+            throw new AppError("Missing user ID")
+        }
+        if (!roleIds || !Array.isArray(roleIds) || roleIds.length === 0) {
+            throw new AppError("Missing role IDs", 400)
+        }
+
+        await userService.setUserRoles(id, roleIds)
+        return successResponse(res, null, "User role updated successfully", 200)
     } catch (e) {
         next(e)
     }
