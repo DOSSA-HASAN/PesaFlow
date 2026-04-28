e14b4c0b19ab7834a80a69c030478768332aabea
Hasan <hassandossa5@gmail.com>
2026-04-28 18:44:40 +0300

    feat: add permission based authorization to routes

--- backend/rbac/permission/permission.route.js
+++ backend/rbac/permission/permission.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import * as permissionController from "./permission.controller.js"
+import { verifyUser } from "../../middlewares/auth.middleware.js";
+import { authorize } from "../../middlewares/checkRole.middleware.js";
 
 const router = express.Router()
-
-router.post('/', permissionController.addPermission)
-router.get('/', permissionController.getAllPermissions)
-router.get('/:id', permissionController.getPermissionById)
-router.delete('/:id', permissionController.deletePermission)
-router.patch('/:id', permissionController.updatePermission)
+router.use(verifyUser)
+router.post('/', authorize(['permission.create']), permissionController.addPermission)
+router.get('/', authorize(['permission.read']), permissionController.getAllPermissions)
+router.get('/:id', authorize(['permission.read']), permissionController.getPermissionById)
+router.delete('/:id', authorize(['permission.delete']), permissionController.deletePermission)
+router.patch('/:id', authorize(['permission.update']), permissionController.updatePermission)
 
 export default router
