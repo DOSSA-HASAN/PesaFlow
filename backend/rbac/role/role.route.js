08f32bad973f511f352c836406f8dd40b18dd514
Hasan <hassandossa5@gmail.com>
2026-04-29 09:19:21 +0300

    feat: add permission based route guards

--- backend/rbac/role/role.route.js
+++ backend/rbac/role/role.route.js
@@ -1,13 +1,16 @@
 import express from "express"
 import * as roleController from "./role.controller.js"
+import { authorize } from "../../middlewares/checkRole.middleware.js"
+import { verifyUser } from "../../middlewares/auth.middleware.js"
 
 const router = express.Router()
-
-router.post('/', roleController.addRole)
-router.get('/', roleController.getAllRoles)
-router.get('/:field', roleController.getRoleByIdOrName)
-router.delete('/:id', roleController.deleteRole)
-router.patch('/:id', roleController.updateRole)
-router.post('/:id/permissions', roleController.assignPermission)
+router.use(verifyUser)
+router.post('/', authorize(['role.create']), roleController.addRole)
+router.get('/', authorize(['role.read']), roleController.getAllRoles)
+router.get('/id/:id', authorize(['role.read']), roleController.getRoleById)
+router.get('/name/:name', authorize(['role.read']), roleController.getRoleByName)
+router.delete('/:id', authorize(['role.delete']), roleController.deleteRole)
+router.patch('/:id', authorize(['role.update']), roleController.updateRole)
+router.post('/:id/permissions', authorize(['role.assign']), roleController.assignPermission)
 
 export default router
