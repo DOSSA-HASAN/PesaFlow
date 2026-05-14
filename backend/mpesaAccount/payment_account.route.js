600499e7549b5cc653f756cbd7811308689a89fa
Hasan <hassandossa5@gmail.com>
2026-05-14 16:55:54 +0300

    feat: added routes to updated, block, & unblock payment account

--- backend/mpesaAccount/payment_account.route.js
+++ backend/mpesaAccount/payment_account.route.js
@@ -5,5 +5,8 @@ const router = express.Router()
 
 router.post("/", paymentController.addPaymentAccount)
 router.get('/', paymentController.getAllPaymentAccounts)
+router.patch('/:id', paymentController.updatePaymentAccount)
+router.patch("/:id/block", paymentController.blockPaymentAccount)
+router.patch("/:id/unblock", paymentController.unblockPaymentAccount)
 
 export default router
