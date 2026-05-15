7b67637394f775e035c3d023206ef8cb07920713
Hasan <hassandossa5@gmail.com>
2026-05-15 09:41:26 +0300

    fix: added verifyUser middleware to ensure RBAC auth middleware works

--- backend/mpesaAccount/payment_account.route.js
+++ backend/mpesaAccount/payment_account.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import * as paymentController from "./payment_account.controller.js"
 import {authorize} from "../middlewares/checkRole.middleware.js";
+import {verifyUser} from "../middlewares/auth.middleware.js";
 
 const router = express.Router()
-
+router.use(verifyUser)
 router.post("/", authorize(["payment.account.create"]), paymentController.addPaymentAccount)
 router.get('/', authorize(["payment.account.view"]), paymentController.getAllPaymentAccounts)
 router.patch('/:id', authorize(["payment.account.update"]), paymentController.updatePaymentAccount)
