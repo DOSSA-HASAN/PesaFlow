58eb208311ec9ce2fc61d7184f0fb137ef309404
Hasan <hassandossa5@gmail.com>
2026-05-15 09:06:09 +0300

    feat: added RBAC authorization middleware

--- backend/mpesaAccount/payment_account.route.js
+++ backend/mpesaAccount/payment_account.route.js
@@ -1,12 +1,13 @@
 import express from "express"
 import * as paymentController from "./payment_account.controller.js"
+import {authorize} from "../middlewares/checkRole.middleware.js";
 
 const router = express.Router()
 
-router.post("/", paymentController.addPaymentAccount)
-router.get('/', paymentController.getAllPaymentAccounts)
-router.patch('/:id', paymentController.updatePaymentAccount)
-router.patch("/:id/block", paymentController.blockPaymentAccount)
-router.patch("/:id/unblock", paymentController.unblockPaymentAccount)
+router.post("/", authorize(["payment.account.create"]), paymentController.addPaymentAccount)
+router.get('/', authorize(["payment.account.view"]), paymentController.getAllPaymentAccounts)
+router.patch('/:id', authorize(["payment.account.update"]), paymentController.updatePaymentAccount)
+router.patch("/:id/block", authorize(["payment.account.update"]), paymentController.blockPaymentAccount)
+router.patch("/:id/unblock", authorize(["payment.account.update"]), paymentController.unblockPaymentAccount)
 
 export default router
