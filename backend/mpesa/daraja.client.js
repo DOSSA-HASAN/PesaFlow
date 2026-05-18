32022c148f40766c72d6df9ccaa04ad1fd7e8daa
Hasan <hassandossa5@gmail.com>
2026-05-18 09:56:33 +0300

    feat: added axios interceptors for automatic mpesa access token handling

--- backend/mpesa/daraja.client.js
+++ backend/mpesa/daraja.client.js
@@ -1,6 +1,31 @@
-import axios from "axios"
+import axios from "axios";
 import "dotenv/config.js"
+import {fetchAccessToken} from "./shared/accessToken.js";
 
-const mpesaDarajaClient = axios.create({
+const darajaClient = axios.create({
     baseURL: process.env.MPESA_BASE_URL
 })
+
+darajaClient.interceptors.request.use(async (config) => {
+    const accessToken = await fetchAccessToken()
+    config.headers.Authorization = `Bearer ${accessToken}`
+
+    return config
+})
+
+darajaClient.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+        const originalRequest = error.config
+        if (error.response.status === 401 && !originalRequest._retry){
+            const accessToken = await fetchAccessToken()
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`
+            originalRequest._retry = true
+            return darajaClient(originalRequest)
+        }
+        return Promise.reject(error)
+    }
+)
+
+
