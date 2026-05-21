3a24e2698bef2acbf21118f8631e49fab93363da
Hasan <hassandossa5@gmail.com>
2026-05-21 08:54:32 +0300

    feat: add route to generate payment QR code

--- backend/mpesa/mpesa.route.js
+++ backend/mpesa/mpesa.route.js
@@ -1,8 +1,11 @@
 import express from "express"
 import {getAccessToken} from "./shared/accessToken.js";
+import * as mpesaController from "./mpesa.controller.js"
+import {paymentAccountResolver} from "../middlewares/paymentAccountResolver.js";
 
 const router = express.Router()
 
 router.post("/token", getAccessToken)
+router.post("/qrcode", paymentAccountResolver, mpesaController.generatePaymentQRCode)
 
 export default router
