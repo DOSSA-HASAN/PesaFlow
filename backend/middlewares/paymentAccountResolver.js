174eba9ec84a393b5b55ccb88f326edb9eb285d1
Hasan <hassandossa5@gmail.com>
2026-05-22 11:41:10 +0300

    feat: check account status from cache and fetch

--- backend/middlewares/paymentAccountResolver.js
+++ backend/middlewares/paymentAccountResolver.js
@@ -5,29 +5,36 @@ import {errorResponse} from "../utils/response.js";
 export const paymentAccountResolver = async (req, res, next) => {
     try {
         const {shortCode} = req.body
-        if(!shortCode){
+        if (!shortCode) {
             return errorResponse(res, "Missing payment account number", 400)
         }
-
         const key = `mpesa:payment_account:${shortCode}`
-
         const cachedPaymentAccount = await redisClient.get(key)
-        if(cachedPaymentAccount){
-            req.paymentAccount = JSON.parse(cachedPaymentAccount)
-            next()
+        const account = JSON.parse(cachedPaymentAccount)
+        if (account) {
+            if (account["isBlocked"] === true) {
+                return errorResponse(res, "This payment account has been blocked.", 400)
+            } else {
+                req.paymentAccount = JSON.parse(cachedPaymentAccount)
+                next()
+            }
         } else {
             const fetchedPaymentAccount = await PaymentAccount.findOne({
                 where: {shortCode}
             })
-            if(!fetchedPaymentAccount){
+            if (!fetchedPaymentAccount) {
                 return errorResponse(res, "Payment account not found")
             }
 
             await redisClient.set(`mpesa:payment_account:${fetchedPaymentAccount.shortCode}`, JSON.stringify(fetchedPaymentAccount), {
-                "EX":3600
+                "EX": 3600
             })
             req.paymentAccount = fetchedPaymentAccount
-            next()
+            if (req.paymentAccount.isBlocked === true) {
+                return errorResponse(res, "This payment account has been blocked.", 400)
+            } else {
+                next()
+            }
         }
     } catch (e) {
         next(e)
