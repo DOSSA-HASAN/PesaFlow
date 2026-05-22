018691256dc1b4617dee15627df272c68f30e01d
Hasan <hassandossa5@gmail.com>
2026-05-22 08:37:31 +0300

    feat: add route to initiate STK prompt

--- backend/mpesa/mpesa.route.js
+++ backend/mpesa/mpesa.route.js
@@ -6,6 +6,14 @@ import {paymentAccountResolver} from "../middlewares/paymentAccountResolver.js";
 const router = express.Router()
 
 router.post("/token", getAccessToken)
-router.post("/qrcode", paymentAccountResolver, mpesaController.generatePaymentQRCode)
+router.post("/qr/generate", paymentAccountResolver, mpesaController.generatePaymentQRCode)
+router.post("/c2b/register-urls", paymentAccountResolver, mpesaController.registerValidationAndConfirmationUrl)
+router.post("/validation", paymentAccountResolver, (req, res, next)=>{
+    console.log("Validation url")
+}) // this route will run a function to validate incoming payment, if payments are valid continue and save to db
+router.post("/confirmation", paymentAccountResolver, (req, res, next) => {
+    console.log("Confirmation url")
+}) // runs after payment processed
+router.post("/stk/initiate", paymentAccountResolver, mpesaController.initiateStkPush)
 
 export default router
