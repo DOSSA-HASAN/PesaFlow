abd565919088cb156e53c0f6f484f01b35459b52
Hasan <hassandossa5@gmail.com>
2026-05-29 12:00:55 +0300

    refactor: change route function

--- backend/mpesa/mpesa.route.js
+++ backend/mpesa/mpesa.route.js
@@ -2,9 +2,11 @@ import express from "express"
 import {getAccessToken} from "./shared/accessToken.js";
 import * as mpesaController from "./mpesa.controller.js"
 import {paymentAccountResolver} from "../middlewares/paymentAccountResolver.js";
+import {initiateStkPush} from "./stk/stk.controller.js";
+import {verifyUser} from "../middlewares/auth.middleware.js";
 
 const router = express.Router()
-
+router.use(verifyUser)
 router.post("/token", getAccessToken)
 router.post("/qr/generate", paymentAccountResolver, mpesaController.generatePaymentQRCode)
 router.post("/c2b/register-urls", paymentAccountResolver, mpesaController.registerValidationAndConfirmationUrl)
@@ -14,7 +16,7 @@ router.post("/validation", paymentAccountResolver, (req, res, next)=>{
 router.post("/confirmation", paymentAccountResolver, (req, res, next) => {
     console.log("Confirmation url")
 }) // runs after payment processed
-router.post("/stk/initiate", paymentAccountResolver, mpesaController.initiateStkPush)
+router.post("/stk/initiate", paymentAccountResolver, initiateStkPush)
 router.post("/b2c/initiate", paymentAccountResolver, mpesaController.initiateB2CPayment)
 
 export default router
