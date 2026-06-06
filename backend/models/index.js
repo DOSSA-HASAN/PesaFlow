12ef140602a23ca6977267653e7dc92bf0df7b25
Hasan <hassandossa5@gmail.com>
2026-06-06 22:15:18 +0300

    feat: add relations for Users & Payments

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,8 @@
+import { sequelize } from "../config/db.js";
 import User from "../user/user.model.js";
 import { Role } from "../rbac/role/role.model.js";
 import { Permission } from "../rbac/permission/permission.model.js";
-import { sequelize } from "../config/db.js";
+import {Payment} from "../payment/payment.model.js";
 
 // Tenant.hasMany(User, {foreignKey: "tenantId", as: "users"})
 // User.belongsTo(Tenant, {foreignKey: "tenantId", as: "tenant"})
@@ -9,9 +10,14 @@ import { sequelize } from "../config/db.js";
 User.belongsToMany(Role, { through: "UserRoles" })
 Role.belongsToMany(User, { through: "UserRoles" })
 
-const RolePermissions = sequelize.define("RolePermission", {}, { timestamps: true })
+const RolePermissions = sequelize.define("role_permission", {}, { timestamps: true })
 
 Role.belongsToMany(Permission, { through: RolePermissions })
 Permission.belongsToMany(Role, { through: RolePermissions })
+
+Payment.belongsTo(User, {foreignKey: "initiatedBy", as: "initiator"})
+Payment.belongsTo(User, {foreignKey: "completedBy", as: "completer"})
+User.hasMany(Payment, {foreignKey: "initiatedBy", as: "initiatedPayment"})
+User.hasMany(Payment, {foreignKey: "completedBy", as: "completedPayment"})
 
 export { User, Role, Permission, RolePermissions }
