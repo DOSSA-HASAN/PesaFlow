f49ed9fa49527bc00b3f1e3e237ea0396bf1c9ac
Hasan <hassandossa5@gmail.com>
2026-06-09 10:26:54 +0300

    feat: function to generate password for daraja requests

---
+++ backend/utils/generateMpesaPassword.js
@@ -0,0 +1,11 @@
+export const generateMpesaPassword = (shortCode) => {
+    if(!shortCode){
+        throw new AppError("Short code required to generate password")
+    }
+    const PASSKEY = process.env.PASSKEY
+    const timestamp = new Date()
+        .toISOString()
+        .replace(/[-T:.Z]/g, "")
+        .slice(0, 14);
+    return Buffer.from(shortCode + PASSKEY + timestamp).toString("base64")
+}
