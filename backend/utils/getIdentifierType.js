5c7e7573236246d915e55ee9a62f00c337df196b
Hasan <hassandossa5@gmail.com>
2026-06-09 10:36:51 +0300

    feat: function to get identifier type for daraja requests

---
+++ backend/utils/getIdentifierType.js
@@ -0,0 +1,17 @@
+import "dotenv/config.js"
+import {AppError} from "./AppError.js";
+
+const IDENTIFIER_TYPES = {
+    BG: "1",
+    TILL: "2",
+    PB: "4"
+}
+export const getIdentifierType = (shortcodeType = process.env.MPESA_SHORTCODE_TYPE) => {
+    const identifierType = IDENTIFIER_TYPES[shortcodeType]
+    console.log(identifierType)
+    if(!identifierType){
+        throw new AppError(`Invalid short code identifier type: ${shortcodeType}`)
+    }
+
+    return identifierType
+}
