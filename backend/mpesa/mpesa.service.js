dbbd76a663a67ab1807450b1f2ac25e1f2824247
Hasan <hassandossa5@gmail.com>
2026-06-13 17:29:19 +0300

    feat: usage of getMpesaEnvironmentSpecificValue

--- backend/mpesa/mpesa.service.js
+++ backend/mpesa/mpesa.service.js
@@ -1,6 +1,8 @@
 import "dotenv/config.js"
 import {AppError} from "../utils/AppError.js";
 import {darajaRequest} from "./shared/darajaRequest.js";
+import {getTrxCode} from "../utils/getTrxCode.js";
+import {getMpesaEnvironmentSpecificValue} from "../utils/getMpesaEnvironmentSpecificValue.js";
 
 const BASE_URL = process.env.MPESA_BASE_URL
 const IS_SANDBOX = process.env.MPESA_ENV === "sandbox"
@@ -32,24 +34,19 @@ export const generatePaymentQRCode = async (shortCode, RefNo = "Invoice Test", a
     }
     try {
         const MerchantName = process.env.MERCHANT_NAME.toString()
-        const TrxCode = process.env.MPESA_SHORTCODE_TYPE.toString()
-        if (!shortCode) {
-            throw new AppError(`Missing ${TrxCode.toString().toLowerCase()}`, 400)
-        }
 
         const method = "POST"
         const url = `/mpesa/qrcode/v1/generate`
         const data = {
-            MerchantName,
-            RefNo,
-            Amount: Number(amount),
-            TrxCode,
-            CPI: shortCode,
-            size
+            "MerchantName": MerchantName,
+            "RefNo": RefNo,
+            "Amount": amount,
+            "TrxCode": getTrxCode(),
+            "CPI": shortCode,
+            "size": size
         }
 
         const qrCode = await darajaRequest({method, url, data})
-        console.log(`QRCODE: ${JSON.stringify(qrCode)}`)
         return qrCode
     } catch (e) {
         throw e
@@ -95,10 +92,15 @@ export const registerValidationAndConfirmationUrl = async (shortCode, confirmati
     const data = {
         "ShortCode": shortCode,
         "ResponseType": "Completed",
-        "ConfirmationURL": confirmationUrl,
-        "ValidationURL": validationUrl
+        "ConfirmationURL": getMpesaEnvironmentSpecificValue("https://mydomain.com/confirmation", confirmationUrl),
+        "ValidationURL": getMpesaEnvironmentSpecificValue("https://mydomain.com/validation", validationUrl)
+    }
+    try {
+        const res = await darajaRequest({method, url, data})
+        return res
+
+    } catch (e) {
+        throw e
     }
 
-    const res = await darajaRequest({method, url, data})
-    return res
 }
