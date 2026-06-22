372ae26b1a1f7c881f8fd09df0b2f10911ede5d4
Hasan <hassandossa5@gmail.com>
2026-06-22 09:42:06 +0300

    feat: pass object as argument to service layer

--- backend/mpesa/b2BuyGoods/b2BuyGoods.controller.js
+++ backend/mpesa/b2BuyGoods/b2BuyGoods.controller.js
@@ -6,7 +6,7 @@ export const b2BuyGoods = async (req, res, next) => {
         const {shortCode} = req.paymentAccount
         const {id} = req.user
         const {amount, recieverShortCode, accountReference, remarks, idempotencyKey} = req.body
-        const paymentResponse = await b2BuyGoodsService.b2BuyGoods(amount, shortCode, recieverShortCode, accountReference, remarks, idempotencyKey, id)
+        const paymentResponse = await b2BuyGoodsService.b2BuyGoods({amount, shortCode, recieverShortCode, accountReference, remarks, idempotencyKey, userId: id})
         return successResponse(res, paymentResponse, "Payment request submitted successfully", 200)
     } catch (e) {
         next(e)
