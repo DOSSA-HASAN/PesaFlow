8bc1c4ea852494831dc791a4bf17113602114fb0
Hasan <hassandossa5@gmail.com>
2026-06-22 09:12:47 +0300

    feat: additional checks if fields are present

--- backend/mpesa/b2c/b2c.controller.js
+++ backend/mpesa/b2c/b2c.controller.js
@@ -6,13 +6,22 @@ export const initiateB2CPayment = async (req, res, next) => {
         const {shortCode} = req.paymentAccount
         const {id} = req.user
         const {commandId, amount, receiver, remarks, idempotencyKey} = req.body
+
+        if (!shortCode) {
+            return errorResponse(res, "Missing short code number", 400)
+        }
+
+        if (!id) {
+            return errorResponse(res, "Missing current session user ID", 400)
+        }
+
         if (!amount || !receiver || !idempotencyKey) {
             return errorResponse(res, "Missing required fields (amount / receiver number / idempotency key)")
         }
-        if (amount <= 0) {
-            return errorResponse(res, "Amount must be more than 0")
+        if (amount === "0" || Number(amount) < 0) {
+            return errorResponse("Amount must be greater than 0", 400)
         }
-        console.log(`USER ID: ${id}`)
+
         const b2cPayment = await b2cService.initiateB2CPayment({
             idempotencyKey,
             commandId,
