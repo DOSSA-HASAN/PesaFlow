fc99887c3acc7614c6c28f4d640ac8588d8fc594
Hasan <hassandossa5@gmail.com>
2026-06-29 19:57:03 +0300

    refactor: changed callback base url

--- backend/mpesa/callbacks/callback.route.js
+++ backend/mpesa/callbacks/callback.route.js
@@ -3,6 +3,6 @@ import {callbackHandler} from "./callback.handler.js";
 
 const router = express.Router()
 
-router.post('/stk/callback', callbackHandler)
+router.post('/payment/callbacks', callbackHandler)
 
 export default router
