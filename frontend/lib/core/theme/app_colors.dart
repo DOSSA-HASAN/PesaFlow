73dff79c735f11b00f66cc393c5a9bb3f7374104
Hasan <hassandossa5@gmail.com>
2026-07-02 23:16:11 +0300

    feat: add app colors

---
+++ frontend/lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF13EC13); // Mpesa green color
+
+  static const Color background = Color(0xFFF6F8F6); // Light gray bg
+  static const Color surface = Colors.white; // card bg
+
+  // Text shades
+  static const Color textPrimary = Color(0xFF171717);
+  static const Color textSecondary = Color(0xFF737373);
+}
