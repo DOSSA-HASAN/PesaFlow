012c5f9171fbae82b326064a5029dc731c53d07d
Hasan <hassandossa5@gmail.com>
2026-07-06 23:18:40 +0300

    refactor: pass dio instance to constructor

--- frontend/lib/features/login/data/repository/login_repository.dart
+++ frontend/lib/features/login/data/repository/login_repository.dart
@@ -3,27 +3,13 @@ import 'package:frontend/core/network/dio_client.dart';
 import 'package:frontend/features/login/data/models/login_request.dart';
 
 class LoginRepository {
-  // preconfigured network instance
-  final Dio _dio = DioClient().instance;
+  final Dio _dio;
 
-  // Api call
-  Future<bool> login(LoginRequest requestDetails) async {
-    try {
-      print("Calling backend");
-      final response = await _dio.post(
-          '/api/auth/login', // TODO: check api route if its correct
-          data: requestDetails.toJson()
-      );
+  LoginRepository(this._dio);
 
-      if(response.statusCode == 200){
-        print("Logged in successfully: ${response}");
-        return true;
-      }
+  Future<Response> login(LoginRequest requestDetails) async {
+    final response = await _dio.post("/auth/login", data: requestDetails);
 
-      return false;
-    } on DioException catch (e){
-      print("❌ An Error occurred while logging in: ${e.message}");
-      return false;
-    }
+    return response;
   }
 }
