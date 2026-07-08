a2284d9178bdde95bbc43fa2df5d7953abfbaf72
Hasan <hassandossa5@gmail.com>
2026-07-08 23:22:13 +0300

    feat: add model for b2buygoods payment screen

---
+++ frontend/lib/features/payment/b2BuyGoods/data/models/b2_buy_goods_request.dart
@@ -0,0 +1,26 @@
+import 'package:uuid/uuid.dart';
+
+class B2BuyGoodsRequest {
+  final String shortCode;
+  final String amount;
+  final String recieverShortCode;
+  final String accountReference;
+  final String idempotencyKey;
+
+  B2BuyGoodsRequest({
+    required this.shortCode,
+    required this.amount,
+    required this.recieverShortCode,
+    required this.accountReference,
+  }) : idempotencyKey = const Uuid().v4();
+
+  Map<String, dynamic> toJson() {
+    return {
+      shortCode: shortCode,
+      amount: amount,
+      recieverShortCode: recieverShortCode,
+      accountReference: accountReference,
+      idempotencyKey: idempotencyKey,
+    };
+  }
+}
