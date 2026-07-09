96e93fb60dafdfe07369da88aa1957e85a198d9a
Hasan <hassandossa5@gmail.com>
2026-07-09 23:13:15 +0300

    feat: add widget for payment callback

--- frontend/lib/features/payment/b2BuyGoods/presentation/screens/b2_buy_goods_screen.dart
+++ frontend/lib/features/payment/b2BuyGoods/presentation/screens/b2_buy_goods_screen.dart
@@ -3,6 +3,7 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:frontend/core/widgets/btn.dart";
 import "package:frontend/core/widgets/text_field.dart";
 import "package:frontend/core/widgets/toast_util.dart";
+import "package:frontend/features/payment/b2BuyGoods/presentation/widgets/b2_buy_goods_payment_response.dart";
 import "package:frontend/features/payment/b2BuyGoods/provider/b2_buy_goods_provider.dart";
 import "package:frontend/features/payment/stk/provider/stk_provider.dart";
 import "package:toastification/toastification.dart";
@@ -36,6 +37,7 @@ class _B2BuyGoodsScreenState extends ConsumerState<B2BuyGoodsScreen> {
               "B2BuyGoods payment has been submitted and is now being processed.",
         );
       } else if (next is AsyncError) {
+        print("b2buygoods screeen errorr");
         ToastUtil.showGeneralToast(
           context: context,
           type: ToastificationType.error,
@@ -62,128 +64,140 @@ class _B2BuyGoodsScreenState extends ConsumerState<B2BuyGoodsScreen> {
           ),
         ],
       ),
-      child: Column(
+      child: Row(
         children: [
           Expanded(
-            child: Container(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.shield_rounded,
-                        color: theme.colorScheme.primary,
-                      ),
-                      const SizedBox(width: 12),
-                      Text(
-                        "SECURE PAYMENT",
-                        style: TextStyle(
-                          color: theme.colorScheme.primary,
-                          fontWeight: FontWeight.bold,
+            flex: 1,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.shield_rounded,
+                              color: theme.colorScheme.primary,
+                            ),
+                            const SizedBox(width: 12),
+                            Text(
+                              "SECURE PAYMENT",
+                              style: TextStyle(
+                                color: theme.colorScheme.primary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    "Business To Buy Goods Transaction",
-                    style: TextStyle(
-                      color: theme.colorScheme.onSecondary.withOpacity(0.8),
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Text(
-                    "All fields are required. Once submitted wait for confirmation notification.",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: theme.colorScheme.onSecondary.withOpacity(0.5),
-                      fontSize: 18,
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.start,
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: CustomTextField(
-                          label: "Payment Amount (KES)",
-                          hintText: "12,450",
-                          prefixIcon: Icon(Icons.money),
-                          controller: _amountController,
-                        ),
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        child: CustomTextField(
-                          label: "Receiver Till Number",
-                          hintText: "600000",
-                          prefixIcon: Icon(Icons.numbers_rounded),
-                          controller: _receiverTillController,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  CustomTextField(
-                    label: "Invoice Number / Account Reference",
-                    hintText: "INV-001-7-7-2026",
-                    controller: _accountRefController,
-                  ),
-                  const SizedBox(height: 20),
-                  CustomButton(
-                    label: _b2BuyGoodsRef is AsyncLoading
-                        ? "Processing payment..."
-                        : "Buy Goods",
-                    width: 400,
-                    height: 50,
-                    onPressed: _b2BuyGoodsRef is AsyncLoading
-                        ? () {}
-                        : () {
-                            //TODO: shortcode is hardcoded for now
-                            ref
-                                .read(b2BuyGoodsProvider.notifier)
-                                .submitB2BuyGoods(
-                                  "600989",
-                                  _amountController.text.trim(),
-                                  _receiverTillController.text.trim(),
-                                  _accountRefController.text.trim(),
-                                );
-                          },
-                  ),
-                  const SizedBox(height: 30),
-                  Flexible(
-                    child: Container(
-                      padding: const EdgeInsets.all(12),
-                      decoration: BoxDecoration(
-                        color: theme.colorScheme.primary.withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(
-                            Icons.info_rounded,
-                            color: theme.colorScheme.primary,
+                        Text(
+                          "Business To Buy Goods Transaction",
+                          style: TextStyle(
+                            color: theme.colorScheme.onSecondary.withOpacity(
+                              0.8,
+                            ),
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
                           ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: Text(
-                              "Payment transaction to buy goods. Once you click the above button to process the payment wait for a confirmation notification.",
-                              softWrap: true,
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          "All fields are required. Once submitted wait for confirmation notification.",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: theme.colorScheme.onSecondary.withOpacity(
+                              0.5,
+                            ),
+                            fontSize: 18,
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        Row(
+                          // crossAxisAlignment: CrossAxisAlignment.start,
+                          // mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: CustomTextField(
+                                label: "Payment Amount (KES)",
+                                hintText: "12,450",
+                                prefixIcon: Icon(Icons.money),
+                                controller: _amountController,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: CustomTextField(
+                                label: "Receiver Till Number",
+                                hintText: "600000",
+                                prefixIcon: Icon(Icons.numbers_rounded),
+                                controller: _receiverTillController,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        CustomTextField(
+                          label: "Invoice Number / Account Reference",
+                          hintText: "INV-001-7-7-2026",
+                          controller: _accountRefController,
+                        ),
+                        const SizedBox(height: 20),
+                        CustomButton(
+                          label: _b2BuyGoodsRef is AsyncLoading
+                              ? "Processing payment..."
+                              : "Buy Goods",
+                          width: 400,
+                          height: 50,
+                          onPressed: _b2BuyGoodsRef is AsyncLoading
+                              ? () {}
+                              : () {
+                                  //TODO: shortcode is hardcoded for now
+                                  ref
+                                      .read(b2BuyGoodsProvider.notifier)
+                                      .submitB2BuyGoods(
+                                        "600989",
+                                        _amountController.text.trim(),
+                                        _receiverTillController.text.trim(),
+                                        _accountRefController.text.trim(),
+                                      );
+                                },
+                        ),
+                        const SizedBox(height: 30),
+                        Flexible(
+                          child: Container(
+                            padding: const EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: theme.colorScheme.primary.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Icon(
+                                  Icons.info_rounded,
+                                  color: theme.colorScheme.primary,
+                                ),
+                                const SizedBox(width: 20),
+                                Expanded(
+                                  child: Text(
+                                    "Payment transaction to buy goods.",
+                                    softWrap: true,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
+          Expanded(child: B2BuyGoodsPaymentResponse())
         ],
       ),
     );
